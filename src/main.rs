c9369c23f1a1aabf4951ba48b1290b2ed75461da
Bastian Pedersen <bastian.tangedal@gmail.com>
2023-11-02 20:24:59 +0100

    Varius code improvements

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,79 @@
 use std::io;
-use std::io::{Read, Write};
+use std::io::Write;
 use reqwest;
 use serde;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 struct Word {
-    word: String
+    word: String,
+
+    #[serde(skip_deserializing)]
+    guessed_chars: Vec<char>
 }
 
-const API_URL: &str = "https://random-word-api.herokuapp.com/word?lang=es";
+const API_URL: &str = "https://random-word-api.herokuapp.com/word?lang=en";
 
 impl Word {
-    fn is_character_in_word(&self, c: char) -> bool {
-        return self.word.chars().any(|a| a == c)
+    fn contains(&self, c: char) -> bool {
+        self.word.chars().any(|a| a == c)
     }
 
-    fn display_word(&self, guessed_chars: &[char]) -> String {
+    fn add_guessed_char(&mut self, c: char) {
+        self.guessed_chars.push(c);
+    }
+
+    fn display_word(&self) -> String {
         self.word
             .chars()
-            .map(|c| if guessed_chars.contains(&c) { c } else { '_' })
+            .map(|c| if self.guessed_chars.contains(&c) { c } else { '_' })
             .collect()
     }
 }
 
 fn main() {
-    let word = get_word();
-    let mut guessed_chars = Vec::new();
+    let mut word = get_word();
 
     println!(
         "Random word acquired! (hint: it is {} characters long). Now you have to guess it!",
-        word.word.len()
+        word.word.chars().count()
     );
 
     loop {
-        println!("{}", word.display_word(&guessed_chars));
-        print!("Guess character (or type 'exit' to quit): ");
+        println!("{}", word.display_word());
+        print!("Guess character (or type '!exit' to quit): ");
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
         input = input.trim().to_string();
 
-        if input == "exit" {
-            println!("The word was: {}", word.word);
+        if input == word.word {
+            println!("Congratulations! You guessed the word: {}.", word.word);
             break;
         }
 
-        if input.len() != 1 {
+        if input == "!exit" {
+            println!("The word was: {}.", word.word);
+            break;
+        }
+
+        if input.chars().count() != 1 {
             println!("Please enter a single character.");
             continue;
         }
 
         let single_char = input.chars().next().unwrap();
-        guessed_chars.push(single_char);
+        word.add_guessed_char(single_char);
 
-        if word.is_character_in_word(single_char) {
+        if word.contains(single_char) {
             println!("{} is in the word.", single_char);
         } else {
             println!("{} is NOT in the word.", single_char);
         }
 
-        if word.display_word(&guessed_chars) == word.word {
-            println!("Congratulations! You guessed the word: {}", word.word);
+        if word.display_word() == word.word {
+            println!("Congratulations! You guessed the word: {}.", word.word);
             break;
         }
     }
